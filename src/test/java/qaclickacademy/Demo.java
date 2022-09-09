7c51a87b5ae1d5a42f2f459582cfd51eb142d1f2
konarkbansal <konark13@gmail.com>
2022-09-09 18:10:27 +0530

    final commit by American guy

---
+++ src/test/java/qaclickacademy/Demo.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class Demo{
+
+	
+	@Test
+	public void DemoTest1()
+	{
+		System.out.println("DemoTest1");
+		
+	}
+	
+	@Test
+	public void DemoTest2()
+	{
+		System.out.println("DemoTest2");
+		
+		
+	}
+	
+}
